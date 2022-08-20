806d2f0e518a67d8bb6556cc81c9075246cf25ee
Youngjun <yjl.2000@hotmail.com>
2022-08-20 16:35:34 -0400

    get random tickers limit 20 finished

--- server/src/routers/index.ts
+++ server/src/routers/index.ts
@@ -1,22 +1,22 @@
-import express from 'express';
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { sign } from "crypto";
+import express from "express";
 const router = express.Router();
-const finnhub = require('finnhub');
-import { supabase, signUp } from './supabase';
+const finnhub = require("finnhub");
+import { supabase, signUp, getTickers } from "./supabase";
 
-<<<<<<< HEAD
-const api_key = finnhub.ApiClient.instance.authentications['api_key'];
-api_key.apiKey = process.env.FINNHUB_API_KEY;
-=======
 const api_key = finnhub.ApiClient.instance.authentications["api_key"];
 api_key.apiKey = process.env.API_KEY;
->>>>>>> 68dc349af3abae342b5afdf3c4dfce77e04b5926
+
 const finnhubClient = new finnhub.DefaultApi();
 
-router.route('/').get((_req, res) => {
+router.route("/").get((_req, res) => {
 	res.send(`<h2>Hello world<h2/>`);
 });
 
-router.get('/companyProfile', (req, res) => {
+router.get("/tickers", getTickers);
+
+router.get("/companyProfile", (req, res) => {
 	const { symbol } = req.query;
 	try {
 		finnhubClient.companyProfile2(
@@ -45,27 +45,10 @@ router.get('/companyProfile', (req, res) => {
 			}
 		);
 	} catch (error) {
-		throw new Error('nah');
+		throw new Error("nah");
 	}
 });
 
-router.post('/signUp', async (req, res) => {
-	res.status(200);
-	try {
-		const { email, password } = req.body;
-
-		const { user, session, error } = await supabase.auth.signUp({
-			email: email,
-			password: password
-		});
-
-		console.log(user, session, error);
-
-		res.status(200);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send();
-	}
-});
+router.post("/signUp", signUp);
 
 module.exports = router;
--- server/src/routers/supabase.ts
+++ server/src/routers/supabase.ts
@@ -1,37 +1,46 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
+import { randomUUID } from "crypto";
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(
-	'https://lxfafscgmmiyoglzuqyc.supabase.co',
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4ZmFmc2NnbW1peW9nbHp1cXljIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjEwMTU5OTQsImV4cCI6MTk3NjU5MTk5NH0.bBeYEtGj73EgYnieWDQ60Ciso7Cwk_8_CQVhke7x4ao'
+	"https://lxfafscgmmiyoglzuqyc.supabase.co",
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4ZmFmc2NnbW1peW9nbHp1cXljIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjEwMTU5OTQsImV4cCI6MTk3NjU5MTk5NH0.bBeYEtGj73EgYnieWDQ60Ciso7Cwk_8_CQVhke7x4ao"
 );
 
-async function signUpWithEmail(email: string, password: string) {
-	return await supabase.auth.signUp({
+function signUpWithEmail(email: string, password: string) {
+	return supabase.auth.signUp({
 		email: email,
-		password: password
+		password: password,
 	});
-	// console.log(user, error);
-	// return { user, error };
 }
 
-const signUp = (req, res) => {
-	res.status(200);
+const getTickers = async (req, res) => {
+	try {
+		const { data, error } = await supabase.rpc("get_tickers");
+		if (error) throw error;
+		console.log(data);
+		res.json({ data }).send();
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+const signUp = async (req, res) => {
 	try {
 		const { email, password } = req.body;
+		console.log(email);
 
 		const { user, session, error } = await supabase.auth.signUp({
 			email: email,
-			password: password
+			password: password,
 		});
+		if (error) throw error;
 
-		console.log(user, session, error);
-
-		res.status(200);
-	} catch (err) {
-		console.error(err);
+		res.status(200).send();
+	} catch (error) {
+		console.error(error);
 		res.status(500).send();
 	}
 };
 
-export { supabase, signUp, signUpWithEmail };
+export { supabase, signUp, signUpWithEmail, getTickers };
