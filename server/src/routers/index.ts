68dc349af3abae342b5afdf3c4dfce77e04b5926
Youngjun <yjl.2000@hotmail.com>
2022-08-20 14:47:19 -0400

    company profile endpoint finished

--- server/src/routers/index.ts
+++ server/src/routers/index.ts
@@ -5,7 +5,7 @@ const finnhub = require("finnhub");
 import { ServerResponse, IncomingMessage } from "http";
 
 const api_key = finnhub.ApiClient.instance.authentications["api_key"];
-api_key.apiKey = process.env.API_KEY_SANDBOX;
+api_key.apiKey = process.env.API_KEY;
 const finnhubClient = new finnhub.DefaultApi();
 
 router.route("/").get((_req, res) => {
@@ -15,12 +15,40 @@ router.route("/").get((_req, res) => {
 router.get("/companyProfile", (req, res) => {
 	const { symbol } = req.query;
 	try {
-		finnhubClient.companyProfile2({ symbol }, (data: any) => {
-			// const logoUrl = data.logo
-			console.log(data.logo);
-		});
+		finnhubClient.companyProfile2(
+			{ symbol },
+			(_error: any, data: any, _response: any) => {
+				const {
+					country,
+					ipo,
+					logo,
+					marketCapitalization,
+					name,
+					ticker,
+					finnhubIndustry,
+				} = data;
+				res.json({
+					country,
+					ipo,
+					logoUrl: logo,
+					marketCap: marketCapitalization,
+					name,
+					ticker,
+					sector: finnhubIndustry,
+					dateFetched: new Date().toISOString().slice(0, 10),
+				});
+				// console.log(data);
+			}
+		);
 
-		res.json({ symbol });
+		// finnhubClient.companyProfile2(
+		// { symbol: "AAPL" },
+		// (error, data, response) => {
+		// console.log(data);
+		// }
+		// );
+
+		// res.json({ symbol });
 	} catch (error) {
 		throw new Error("nah");
 	}
