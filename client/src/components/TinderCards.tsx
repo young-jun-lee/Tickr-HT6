7fb7cedcfd46d2df1329e51783758e384349c01f
seneli <seneli.sene@gmail.com>
2022-08-20 20:57:51 -0400

    added fields for card 1

--- client/src/components/TinderCards.tsx
+++ client/src/components/TinderCards.tsx
@@ -1,32 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import TinderCard from 'react-tinder-card';
 import 'styles/TinderCards.css';
-import TickrLogo from 'media/tickr_logo.png';
 
 interface Stock {
-	name: string;
-	url: string;
+	logo: string;
+	ticker: string;
+	index: string;
+	sector: string;
+	marketCap: string;
 }
 
 function TinderCards() {
-	const [people, setPeople] = useState([]);
+	const [stocks, setStocks] = useState<Stock[]>([]);
 
 	useEffect(() => {
-		setPeople([
+		// make axios call to back end
+		// call should return a list of 50 stocks in the following format!
+		setStocks([
 			{
-				name: 'person1',
-				url: 'some url'
+				logo: 'https://www.freepnglogos.com/uploads/flour-png/flour-flower-png-transparent-png-images-pluspng-2.png',
+				ticker: 'fake ticker',
+				index: 'fake index',
+				sector: 'fake sector',
+				marketCap: 'fake market cap'
 			},
 			{
-				name: '2',
-				url: '2'
+				logo: 'https://www.freepnglogos.com/uploads/flour-png/flour-flower-png-transparent-png-images-pluspng-2.png',
+				ticker: 'fake ticker 2',
+				index: 'fake index 2',
+				sector: 'fake sector 2',
+				marketCap: 'fake market cap 2'
 			}
 		]);
-		// database
-		// 	.collection('people')
-		// 	.onSnapshot((snapshot) =>
-		// 		setPeople(snapshot.docs.map((doc) => doc.data()))
-		// 	);
 	}, []);
 
 	const swiped = (direction: string, nameToDelete: string) => {
@@ -40,19 +45,26 @@ function TinderCards() {
 	return (
 		<div className='tinderCards'>
 			<div className='tinderCards__cardContainer'>
-				{people.map((person) => (
+				{stocks.map((stock) => (
 					<TinderCard
 						className='tinderCards__swipe'
-						key={person.name}
+						key={stock.ticker}
 						preventSwipe={['up', 'down']}
-						onSwipe={(dir) => swiped(dir, person.name)}
-						onCardLeftScreen={() => outOfFrame(person.name)}
+						onSwipe={(dir) => swiped(dir, stock.ticker)}
+						onCardLeftScreen={() => outOfFrame(stock.ticker)}
 					>
 						<div
 							className='tinderCards__card'
-							style={{ backgroundImage: `url(${person.url})` }}
+							// style={{ backgroundImage: `url(${stock.logo})` }}
 						>
-							<h3>{person.name}</h3>
+							<img
+								className='stock__logo'
+								src={stock.logo}
+								alt={`${stock.ticker} logo`}
+							/>
+							<h3>{stock.ticker}</h3>
+							<p>{`${stock.index} | ${stock.sector}`}</p>
+							<p>{stock.marketCap}</p>
 						</div>
 					</TinderCard>
 				))}
